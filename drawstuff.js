e0ba567b0e2409b79c8e3cc0c9cb0051df59a374
Zack Brooks <213220875+z0brooks@users.noreply.github.com>
2026-08-24 13:55:00 -0400

    Change rectangle corner colors and positions

--- drawstuff.js
+++ drawstuff.js
@@ -158,13 +158,13 @@ function main() {
     var imagedata = context.createImageData(w,h);
  
     // Define a rectangle in 2D with colors and coords at corners
-    var ulc = new Color(255,0,0,255); // upper left corner color: red
-    var urc = new Color(0,255,0,255); // upper right corner color: green
-    var llc = new Color(0,0,255,255); // lower left corner color: blue
+    var ulc = new Color(255,255,0,255); // upper left corner color: yellow
+    var urc = new Color(0,255,255,255); // upper right corner color: cyan
+    var llc = new Color(255,0,255,255); // lower left corner color: magenta
     var lrc = new Color(0,0,0,255); // lower right corner color: black
     var ulx = 50, uly = 50; // upper left corner position
     var urx = 200, ury = 50; // upper right corner position
-    var llx = 50, lly = 150; // lower left corner position
+    var llx = 200, lly = 150; // lower left corner position
     var lrx = 200, lry = 150; // lower right corner position
     
     // set up the vertical interpolation
